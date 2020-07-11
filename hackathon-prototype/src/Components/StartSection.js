caf344ce920fb00ded0308e099c8066acf2c0b6d
Annie Elizabeth Freeman <annie.e.freeman@gmail.com>
2020-07-11 12:53:19 +1200

    Simple home page setup

---
+++ hackathon-prototype/src/Components/StartSection.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function StartSection() {
+  return (
+    <div>
+      <div className="parent-div-start">
+        <div className="welcome-text">Welcome!</div>
+      </div>
+
+      <div className="parent-div-start">
+        <div className="sub-text">Lorem ipsum dolor sit amet</div>
+      </div>
+    </div>
+  );
+}
+
+export default StartSection;
---
+++ hackathon-prototype/src/Components/VisualKeySection.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function VisualKeySection() {
+  return (
+    <div>
+      <div className="parent-div-key">
+          Find resources relevant to you using these colours:
+        <div className="key-text-dev">Resources for Developers</div>
+        <div className="key-text-creator">Resources for Creators</div>
+        <div className="key-text-both">Resources for Both</div>
+      </div>
+
+     
+        
+      </div>
+
+  );
+}
+
+export default VisualKeySection;
